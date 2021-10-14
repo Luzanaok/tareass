2bc1b23658dfb1929bfbc79c00a011a88862b83f
Luzanaok <91968705+Luzanaok@users.noreply.github.com>
2021-10-14 19:57:19 -0300

    hacelo úta

---
+++ src/index3.ts
@@ -0,0 +1,14 @@
+let btnEnv = document.getElementById("btnEnv");
+btnEnv.addEventListener("click", () => {
+//info
+let nombre : boolean ="nombre"
+let claveU : boolean ="claveJuan"
+
+let ingU : 
+//console.log("Bienvenido")
+if (nombre&&claveU) {
+  console.log ("Bienvenido")
+} else {
+  console.log ("Usted no puede ingresar")
+}
+});
